905444251351b8bed5398cbe2368deeab069f2b5
Matthew <matthewmarshdev@gmail.com^C>
2018-02-28 10:52:19 -0700

    first commit

---
+++ routing/apiRoutes.js
@@ -0,0 +1,7 @@
+//importing data and manip. tables. in controller, working with model 
+
+//class ex say modules.exports=function
+
+//lookup express.router instead to use in lieu of. this will help get routes to connect that are in different folders.
+
+//
---
+++ routing/htmlRoutes.js
@@ -0,0 +1,1 @@
+//here is where i require path. doesnt need to be top level. 
