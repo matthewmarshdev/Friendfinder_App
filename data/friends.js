c677a9ded180b062481997e8e4b6c51434f47940
matthewmarshdev <matthewmarshdev@gmail.com>
2018-03-01 11:30:10 -0700

    thursday commit

---
+++ data/friends.js
@@ -0,0 +1,8 @@
+var friendData = [
+  {
+    friendName: "Testie",
+    friendPhoto:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg ",
+    friendScores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+  }
+];
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,7 +1,25 @@
 //importing data and manip. tables. in controller, working with model 
 
+var path = require("path");
+
 //class ex say modules.exports=function
 
 //lookup express.router instead to use in lieu of. this will help get routes to connect that are in different folders.
 
-//
+var friendData = require("../data/friendData");
+
+module.exports = function(app) {
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendData);
+  });
+
+  app.post("/api/friends", function(req, res) {
+
+    /** logic example 
+    if (tableData.length < 5) {
+      tableData.push(req.body);
+      res.json(true);
+    } */
+  
+  });
